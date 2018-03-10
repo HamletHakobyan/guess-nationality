84222066c07bc820f589459ae4cb7601b7f26c12
Hamlet Hakobyan <hamlet.hakobyan@outlook.com>
2018-03-10 16:43:14 +0400

    Added solution and simple domain model.

---
+++ Geeks.GuessNationality.Domain/Models/Nationality.cs
@@ -0,0 +1,10 @@
+﻿namespace Geeks.GuessNationality.Domain.Models
+{
+    public enum Nationality
+    {
+        Japanese,
+        Chinese,
+        Korean,
+        Thai
+    }
+}
---
+++ Geeks.GuessNationality.Domain/Models/Picture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geeks.GuessNationality.Domain.Models
+{
+    class Picture
+    {
+        public int Id { get; set; }
+        public Nationality Nationality { get; set; }
+    }
+}
